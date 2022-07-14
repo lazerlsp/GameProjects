20a8d7485e3f098e93499829b461b267f89413a4
Alex Lazer <lazerlsp@gmail.com>
2022-07-14 17:43:46 +0300

    0.0.1

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>New Game</title>
+    <!--Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
+    <!--CSS-->
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+</head>
+<body>
+
+<header class="top_bar">
+    <img id="logo" src="img/logo.png" alt="logo">
+    <a class="top_btn" id="forum" href="#">Форум</a>
+    <a class="top_btn" id="group" href="#">Группа ВК</a>
+    <a class="top_btn" id="rules" href="#">Правила</a>
+    <a class="top_btn" id="news" href="#">Новости</a>
+    <a class="top_btn" id="autr" href="#">Вход</a>
+</header>
+
+<main>
+    <span id="main_logo_text">Начни свое приключение здесь!</span>
+    <img id="main_logo" src="img/bg.jpg" alt="background">
+
+    <h2 id="online_text">Онлайн</h2>
+    <div class="main_bar" id="online"></div>
+
+    <h2 id="popularity_text">Зал славы</h2>
+    <div class="main_bar" id="popularity"></div>
+
+    <h2 id="events_text">События</h2>
+    <div class="main_bar" id="events"></div>
+</main>
+
+<footer>
+    <h1>Все права защищены!</h1>
+</footer>
+
+</body>
+</html>
